d74df92be4771f4f5dc3c4836bdf468c140da9b0
Maiko <rnaico79@gmail.com>
2024-03-10 17:53:27 -0400

    Chnge config

--- schedule/settings.py
+++ schedule/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = "schedule.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        # "ENGINE": "django.db.backends.sqlite3",
+        # "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "maiko25_django_calendar",
+        "USER": "maiko25_django",
+        "PASSWORD": "Eb*266o7z",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
